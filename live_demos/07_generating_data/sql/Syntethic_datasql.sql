421424710736b1c696324b676a4fe7493dc6ac0f
Nirre23 <Niclas.Karlman@hotmail.com>
2024-11-25 12:59:40 +0100

    update

--- live_demos/07_generating_data/sql/Syntethic_datasql.sql
+++ live_demos/07_generating_data/sql/Syntethic_datasql.sql
@@ -1,23 +1,8 @@
-SELECT UNNEST 
-	(generate_series(1,
-	100)) as id;
+-- RANDOM
+SELECT RANDOM();
 
-SELECT
-	FLOOR(random()*6+1) as dice
-from generate_series(100);
+SELECT * FROM generate_series (9)
 
-SELECT
-	id,
-	'student_' || id as student,
-	score,
-	case
-		when score < 50 then 'F'
-		when score > 75 then 'E'
-		else 'D'
-	end as grade
-from( select
-	UNNEST (generate_series(100)) as id,
-	round(random()* 100) as score,
-);	
-	
-	
+SELECT FLOOR( RANDOM()*6)+1 as dice from generate_series(10);
+
+--timeseries 

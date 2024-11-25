5a6c197e03c627699f08703a8299a02604448dac
Nirre23 <Niclas.Karlman@hotmail.com>
2024-11-25 12:26:28 +0100

    update

--- live_demos/05_filtering/sql/where_clause.sql
+++ live_demos/05_filtering/sql/where_clause.sql
@@ -28,5 +28,59 @@ FROM
 
 SELECT COUNT()  
 
+SELECT * FROM main.data_jobs dj ;
+
+SELECT
+	count(*) as lower_50k
+from
+	main.data_jobs dj
+WHERE
+	salary_in_usd < 50000; 
+
+SELECT * FROM main.data_jobs dj ;
+
+SELECT 
+	CASE
+		WHEN experience_level = 'SE' THEN 'Senior'
+		WHEN experience_level = 'MID' THEN 'Mid level'
+		WHEN experience_level = 'EN' THEN 'Entry level'
+		WHEN experience_level = 'EX' THEN 'Expert'
+	END as experience_label,
+	* exclude (experience_level)
+FROM
+		main.data_jobs dj ;
+
+UPDATE
+	main.data_jobs
+SET 
+	experience_level = CASE
+		WHEN experience_level = 'SE' THEN 'Senior'
+		WHEN experience_level = 'MID' THEN 'Mid level'
+		WHEN experience_level = 'EN' THEN 'Entry level'
+		WHEN experience_level = 'EX' THEN 'Expert'
+	END;
+
+SELECT DISTINCT experience_level FROM main.data_jobs dj ;
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
